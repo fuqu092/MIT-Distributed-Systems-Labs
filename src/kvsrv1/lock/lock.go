bcc763916e94d81c3793a1c72e9f7bbcde6600d3
fuqu092 <kunal_b@cs.iitr.ac.in>
2026-06-03 20:49:48 +0000

    Implementing a lock using key/value clerk with reliable network

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,8 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -9,8 +10,9 @@ type Lock struct {
 	// the specific Clerk type of ck but promises that ck supports
 	// Put and Get.  The tester passes the clerk in when calling
 	// MakeLock().
-	ck kvtest.IKVClerk
-	// You may add code here
+	ck          kvtest.IKVClerk
+	currVersion rpc.Tversion
+	lockname    string
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -20,15 +22,39 @@ type Lock struct {
 // lockname argument; locks with different names should be
 // independent.
 func MakeLock(ck kvtest.IKVClerk, lockname string) *Lock {
-	lk := &Lock{ck: ck}
-	// You may add code here
+	lk := &Lock{ck: ck, lockname: lockname}
 	return lk
 }
 
 func (lk *Lock) Acquire() {
-	// Your code here
+	_, version, err := lk.ck.Get(lk.lockname)
+	if err == rpc.ErrNoKey {
+		lk.currVersion = 0
+	} else if version%2 == 1 {
+		lk.currVersion = version + 1
+	} else {
+		lk.currVersion = version
+	}
+
+	for {
+		err := lk.ck.Put(lk.lockname, lk.lockname, lk.currVersion)
+		if err == rpc.OK {
+			break
+		}
+
+		_, version, _ = lk.ck.Get(lk.lockname)
+		if lk.currVersion > version {
+			continue
+		} else if version%2 == 1 {
+			lk.currVersion = version + 1
+		} else {
+			lk.currVersion = version
+		}
+	}
+
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	lk.ck.Put(lk.lockname, lk.lockname, lk.currVersion+1)
 }
